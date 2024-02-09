fbea7d37d30039facad5d166d8da6000a8e21b91
DimitryAujour <106028495+DimitryAujour@users.noreply.github.com>
2024-02-09 17:34:03 -0500

    first

---
+++ js/app.js
@@ -0,0 +1,61 @@
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
+
+setTimeout(function(){
+  modal.style.display = 'inline'
+}, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+  modal.style.display = 'none'
+})
+
+declineBtn.addEventListener('mouseenter', function(){
+  modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+})
+
+/*
+Challenge:
+1. Take control of the div holding the buttons.
+2. In index.css, set up a selector for a new class
+   that uses flex-direction to reverse the order
+   of its child elements.
+3. Toggle that class on the div holding the buttons
+   when a user's cursor hovers over the decline button.
+*/
+
+
+consentForm.addEventListener('submit', function(e){
+  e.preventDefault()
+
+  const consentFormData = new FormData(consentForm)
+  const fullName = consentFormData.get('fullName')
+
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="./img/loading.svg" class="loading" alt="">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`
+
+  setTimeout(function(){
+    document.getElementById('upload-text').innerText = `
+        Making the sale...`
+  }, 1500)
+
+
+  setTimeout(function(){
+    document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>Merry will be 23 and rich MUHAHHAHAH!!</p>
+        <div class="idiot-gif">
+            <img src="img/pirate.gif" alt="">
+        </div>
+          <a href="./index2.html" class="button">OFF YOU GO!</a>
+
+    `
+  }, 3000)
+
+})
